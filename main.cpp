2127b3b976ee81aa01b5f1a854e1c089b38e56ae
Hazem Abuelalla <hazem020906@ucegypt.edu>
2026-06-24 17:20:13 +0300

    new commit

--- main.cpp
+++ main.cpp
@@ -4,36 +4,31 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 
+
+int n=0;
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    
-    QWidget window;
-    window.setWindowTitle("Exercise 1");
+     QWidget window;
+     window.setWindowTitle("Exercise 1");
 
-    
-    QLabel* label = new QLabel("0", &window);
-    label->setAlignment(Qt::AlignCenter);
 
-    
+     QVBoxLayout* layout = new QVBoxLayout(&window);
+     QLabel* label = new QLabel("0", &window);
+     QPushButton* button = new QPushButton("Increment", &window);
 
-    QPushButton* button = new QPushButton("Increment", &window);
+     window.setLayout(layout);
 
-    
-    QVBoxLayout* layout = new QVBoxLayout(&window);
-    layout->addWidget(label);
-    layout->addWidget(button);
+     layout->addWidget(label);
+     layout->addWidget(button);
 
-    
-    static int n = 0;
 
-    
-    QObject::connect(button, &QPushButton::clicked, [label]() {
+     QObject::connect(button, &QPushButton::clicked, [label]() {
         n++;
         label->setText(QString::number(n));
     });
 
-   
     window.show();
 
     return app.exec();
